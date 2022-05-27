a96d73904b21098f0c5766610b7b133d3c821c74
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-05-27 13:24:03 -0500

    Adding css into the main global css file and website

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { BiHome, BiUser, BiBookBookmark, BiBriefcase, BiMoon} from "react-icons/bi";
 
 export default function MainPage() {
   return (
@@ -9,6 +9,44 @@ export default function MainPage() {
         <title>Joshua</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header className={styles.header}>
+        <nav className={styles.container}>
+          <a href="#" className={styles.nav__logo}>Joshua</a>
+
+          <div className={styles.nav__menu}>
+            <ul className={styles.nav__list}>
+              <li className={styles.item}>
+                <a href="#home" className={styles.nav__link}>
+                  <BiHome/>
+                </a>
+              </li>
+
+              <li className={styles.item}>
+                <a href="#about" className={styles.nav__link}>
+                  <BiUser/>
+                </a>
+              </li>
+
+              <li className={styles.item}>
+                <a href="#skills" className={styles.nav__link}>
+                  <BiBookBookmark/>
+                </a>
+              </li>
+
+              <li className={styles.item}>
+                <a href="#work" className={styles.nav__link}>
+                  <BiBriefcase/>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/**The change button */}
+          <i className={styles.changeTheme}>
+            <BiMoon/>
+          </i>
+        </nav>
+      </header>
       <main>
         <section className={styles.main}>
           <div className={styles.container}>
