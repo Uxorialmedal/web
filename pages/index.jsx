b8ad596a4aba7e395172be33a8e3ecac029b9f6b
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-05-27 00:06:25 -0500

    Uplaoding a setup of the website

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,13 +2,29 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function MainPage() {
   return (
     <div>
       <Head>
         <title>Joshua</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-    <div>Joshua website</div>
+      <main>
+        <section className={styles.main}>
+          <div className={styles.container}>
+            <span className={styles.greeting}>👋🏻 Hey! I am</span>
+              <h1 className={styles.name}>Joshua Fuentes</h1>
+              <h3 className={styles.education}>3D Designer and NFT Artist</h3>
+
+              <div className={styles.buttons}>
+                <a download= "" href="" className={styles.button}>
+                  Download CV
+                </a>
+              </div>
+          </div>
+        </section> 
+      </main>
+
     </div>
   )
 }
