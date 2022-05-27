7c996b96a02187f0a6652ad7f3e4e9c0caaf4762
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-05-26 23:48:46 -0500

    Adding section to Next.js

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,11 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+    <div>
+      <Head>
+        <title>Joshua</title>
+      </Head>
     <div>Joshua website</div>
-  
+    </div>
   )
 }
