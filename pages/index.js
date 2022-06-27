502660802e440361280b1d0d306ba3fe459df72e
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-06-27 15:00:03 -0500

    Adding description

--- pages/index.js
+++ pages/index.js
@@ -13,19 +13,24 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Joshua Fuentes <a href="https://nextjs.org">3D Designer</a>
+          Joshua Fuentes <a href="https://twitter.com/eluxorialmedal4">3D Designer</a>
         </h1>
+
+        <div className={styles.description}>
+          <p>I´m 3D desginer that loves create new things with Blender 💻</p>
+
+          <p>The following images was created for me using [...]</p>
+        </div>
+
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Loving game development 🎮
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
